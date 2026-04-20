87cbea3e29a67774cc9503e602efddfc0e53a535
Leonardo Lima de Vasconcellos <leonardolimadevasconcellos@gmail.com>
2026-04-20 11:20:33 -0300

    chore: remove google maps

--- components/google-map.tsx
+++ components/google-map.tsx
@@ -1,12 +1,3 @@
 export default function GoogleMap() {
-  return (
-    <iframe
-      src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3576.850995923505!2d-48.851310188477946!3d-26.298930017743842!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94deb0379e31b397%3A0xd89d0f4b5ccfcec9!2sR.%20M%C3%A1rio%20Lobo%2C%20106%20-%20Centro%2C%20Joinville%20-%20SC%2C%2089201-330!5e0!3m2!1spt-BR!2sbr!4v1776601383432!5m2!1spt-BR!2sbr'
-      width='100%'
-      height='300'
-      style={{ border: 0 }}
-      allowFullScreen
-      loading='lazy'
-      referrerPolicy='no-referrer-when-downgrade'></iframe>
-  );
+  return <></>;
 }
